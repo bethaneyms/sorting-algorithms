b53cd77b38c370439b22fed4261cdcc93b6a3c18
bethaneyms <bethaneym03@gmail.com>
2025-02-06 21:02:14 -0600

    Create InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,14 @@
+public class InsertionSort {
+    public int[] sorty(int[] input) {
+        for (int i = 1; i < input.length; i++) {
+            int key = input[i];
+            int j = i - 1;
+            while (j >= 0 && input[j] > key) {
+                input[j + 1] = input[j];
+                j--;
+            }
+            input[j + 1] = key;
+        }
+        return input;
+    }
+}
