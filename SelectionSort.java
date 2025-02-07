6493e18de516a4644292aec2d50efc0f7fca0ee0
bethaneyms <bethaneym03@gmail.com>
2025-02-06 21:02:53 -0600

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,16 @@
+public class SelectionSort {
+    public int[] sorty(int[] input) {
+        for (int i = 0; i < input.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[j] < input[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = input[minIdx];
+            input[minIdx] = input[i];
+            input[i] = temp;
+        }
+        return input;
+    }
+}
